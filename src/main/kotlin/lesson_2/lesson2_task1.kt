8dd2dbd51fb4daeb7e8566fb53d20d2b229178f0
Lastbaks <missouri@inbox.ru>
2024-06-12 11:45:05 +0300

    KS-2-1 calculate grade point average

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    var student01Gradle: Float = 3f
+    var student02Gradle: Float = 4f
+    var student03Gradle: Float = 3f
+    var student04Gradle: Float = 5f
+    var gradlePointAverage: Float = (student01Gradle + student02Gradle + student03Gradle + student04Gradle) / 4
+
+    println(gradlePointAverage)
+}
