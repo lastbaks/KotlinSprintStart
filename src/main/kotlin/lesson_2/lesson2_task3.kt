3c372e4e08f6b222bc385b85f6dacc16bc3864aa
Lastbaks <missouri@inbox.ru>
2024-06-13 21:48:43 +0300

    KS-2-3 add 24 and 60 to constants, optimize calculating

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
+const val MINUTE_IN_HOUR: Byte = 60
+const val HOURS_IN_DAY: Byte = 24
+
 fun main() {
     val departHour: Int = 9
     val departMinute: Int = 39
     val travelTime: Int = 457
 
-    val travelHours = travelTime / 60
-    val travelMinutes = travelTime % 60
-    val extraHour = (departMinute + travelMinutes) / 60
-    val arrivalMinute = (departMinute + travelMinutes) % 60
-    val arrivalHour = (departHour + travelHours + extraHour) % 24
-
+    val minutes: Int = departHour * MINUTE_IN_HOUR + departMinute + travelTime
+    val arrivalHour = minutes / MINUTE_IN_HOUR % HOURS_IN_DAY
+    val arrivalMinute = minutes % MINUTE_IN_HOUR
     println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
