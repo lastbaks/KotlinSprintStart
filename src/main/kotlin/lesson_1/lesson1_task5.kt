0a3eb1a38dac24c2cc48ab8b1abecf3736271f24
Lastbaks <missouri@inbox.ru>
2024-06-13 18:52:07 +0300

    KS-1-5 change 60 and 3600 to const and 6480 to val

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_1
+const val SECONDS_IN_HOUR: Short = 3600
+const val SECONDS_IN_MINUTE: Byte = 60
 
-const val  TOTAL_SECONDS: Short = 6480
 
 fun main() {
 
-    val hours: Int = TOTAL_SECONDS / 3600
-    val minutes: Int = (TOTAL_SECONDS / 60) % 60
-    val seconds: Int = TOTAL_SECONDS % 60
+    val totalSeconds: Short = 6480
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val minutes: Int = (totalSeconds / SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
+    val seconds: Int = totalSeconds % SECONDS_IN_MINUTE
 
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
