3d7709d49e19ff1bd85bdefa791b480e40a4c863
Lastbaks <missouri@inbox.ru>
2024-06-13 22:31:57 +0300

    KS-3-1 print message

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main () {
+    var message: String
+    var name: String
+
+    message = "Добрый день"
+    name = "Олег"
+    println("$message, $name")
+    message = "Добрый вечер"
+    println("$message, $name")
+}
