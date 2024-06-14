446ab2af54fb85297db1efa6411fe8a889dfa14d
Lastbaks <missouri@inbox.ru>
2024-06-14 13:57:02 +0300

    KS-4-2 Check average type of cargo

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_4
+
+fun main() {
+    var cargoWeight: Int = 20
+    var cargoVolume: Int = 80
+
+    println(
+        "Груз с ввесом $cargoWeight килограмм " +
+                "и объемом $cargoVolume литров " +
+                "соответствует категории 'Average': " +
+                "${(cargoWeight > 30) 
+                        and (cargoWeight <= 100) 
+                        and (cargoVolume < 100)}"
+    )
+
+    cargoWeight = 50
+    cargoVolume = 100
+
+    println(
+        "Груз с ввесом $cargoWeight килограмм " +
+                "и объемом $cargoVolume литров " +
+                "соответствует категории 'Average': " +
+                "${(cargoWeight > 30)
+                        and (cargoWeight <= 100)
+                        and (cargoVolume < 100)}"
+    )
+
+}
