af1972575ef5ffbddc86e150a85a34876bffa64e
Lastbaks <missouri@inbox.ru>
2024-06-15 01:03:03 +0300

    KS-4-4 ашетуыы exercises plan

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_4
+
+fun main() {
+    val trainingDay = 8
+    val message: String = """ 
+        |Упражнения для рук:    ${trainingDay % 2 == 1}
+        |Упражнения для ног:     ${trainingDay % 2 == 0}
+        |Упражнения для спины:   ${trainingDay % 2 == 0}
+        |Упражнения для пресса: ${trainingDay % 2 == 1}
+    """.trimMargin()
+
+    println(message)
+}
