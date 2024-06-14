1f68697e887de12e5d58fea9a895790a71581b27
Lastbaks <missouri@inbox.ru>
2024-06-15 00:38:29 +0300

    KS-3-5 parsing String using .split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,11 +2,9 @@ package org.example.lesson_3
 
 fun main() {
     val movingData: String = "D2-D4;0"
-    val startPosition: String = movingData.substring(0,2)
-    val endPosition: String = movingData.substring(3,5)
-    val moveNumber: String = movingData.substring(6)
 
-    println(startPosition)
-    println(endPosition)
-    println(moveNumber)
+    val parsedMovingData = movingData.split("-", ";")
+    println(parsedMovingData[0])
+    println(parsedMovingData[1])
+    println(parsedMovingData[2])
 }
