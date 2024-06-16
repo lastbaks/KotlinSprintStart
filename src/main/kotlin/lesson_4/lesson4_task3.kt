5d80850a9071353c7c55507a0009d76c58f2776f
Lastbaks <missouri@inbox.ru>
2024-06-16 08:39:54 +0300

    KS-4-3 naming correction

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,15 +1,18 @@
 package org.example.lesson_4
 
+const val SUITABLE_HUMIDITY: Int = 20
+const val NON_SUITABLE_SEASON: String = "зима"
+
 fun main() {
-    var sunnyWeather: Boolean = true
-    var openTent:Boolean = true
+    var isSunny: Boolean = true
+    var isAwningOpen:Boolean = true
     var airHumidity: Int = 20
     var season: String = "зима"
 
     // т.к. в этой задаче в отличие от предыдущейнет требования делать сравнение непосредственно в методе println
     // - я вывел сравнение в отдельную переменную для лучшей читаемости,
     // ну и просто для проверки разных подходов к решению
-    val conditionsForLegumes: Boolean = (sunnyWeather && openTent && airHumidity == 20 && season != "зима" )
+    val conditionsForLegumes: Boolean = (isSunny && isAwningOpen && airHumidity == SUITABLE_HUMIDITY && season != NON_SUITABLE_SEASON )
 
     println("Благоприятные ли сейчас условия для роста бобовых? $conditionsForLegumes")
 }
