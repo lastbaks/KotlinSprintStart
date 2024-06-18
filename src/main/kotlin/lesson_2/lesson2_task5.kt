759b604e35b5bdfea5a08d9ed5cc6db8464bb41e
Lastbaks <missouri@inbox.ru>
2024-06-18 18:30:08 +0300

    KS-2-5 Вывод процентной ставки в переменную

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,7 +5,8 @@ import kotlin.math.pow
 
 fun main() {
     val deposit = 70000
-    val sum = deposit * ((1 + 0.167).pow(20))
+    val procentRate = 16.7
+    val sum = deposit * ((1 + (procentRate * 0.01)).pow(20))
     val roundSum = sum.toBigDecimal().setScale(3, RoundingMode.DOWN).toDouble()
     println(roundSum)
 }
