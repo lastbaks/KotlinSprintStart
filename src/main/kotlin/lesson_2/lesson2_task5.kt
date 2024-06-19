20d2e98956687c6c25bc85313cf449975bac2789
Lastbaks <missouri@inbox.ru>
2024-06-19 07:17:02 +0300

    KS-4-5 количество лет добавлено в переменные

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -6,7 +6,8 @@ import kotlin.math.pow
 fun main() {
     val deposit = 70000
     val procentRate = 16.7
-    val sum = deposit * ((1 + (procentRate * 0.01)).pow(20))
+    val countOfYears = 20
+    val sum = deposit * ((1 + (procentRate * 0.01)).pow(countOfYears))
     val roundSum = sum.toBigDecimal().setScale(3, RoundingMode.DOWN).toDouble()
     println(roundSum)
 }
