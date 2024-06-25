5fbc665e88dff57919e44c0d099f5303a7f42668
Lastbaks <missouri@inbox.ru>
2024-06-25 13:04:09 +0300

    KS-6-2 вариант таймера без цикла while

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,14 +1,11 @@
 package org.example.lesson_6
 
 fun main() {
-    var countOfSeconds: Int
+    var countOfSeconds: Long
     val message: String
     println("Введите количество секунд, чтобы запустить таймер")
-    countOfSeconds = readln().toInt()
+    countOfSeconds = readln().toLong()
     message = "Прошло $countOfSeconds секунд"
-    while (countOfSeconds > 0) {
-        countOfSeconds--
-        Thread.sleep(1000)
-    }
+    Thread.sleep(countOfSeconds * 1000)
     println(message)
 }
