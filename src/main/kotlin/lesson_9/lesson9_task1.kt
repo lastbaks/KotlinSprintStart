4c5a9d1ce9da573c80ba24482872a6fc81dcfe4c
Lastbaks <missouri@inbox.ru>
2024-06-27 06:58:55 +0300

    KS-9-1 создание списка ингридиентов с помощью списка

---
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_9
+
+fun main() {
+    val listOfIngredients = listOf("колбаса", "огурец", "горошек", "майонез", "яблоко")
+    listOfIngredients.forEach{
+        println(it)
+    }
+}
