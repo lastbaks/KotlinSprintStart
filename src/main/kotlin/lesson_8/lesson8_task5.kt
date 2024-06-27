cec6270069c4dd506c820da8bb3eaf8921a5d7ef
Lastbaks <missouri@inbox.ru>
2024-06-27 23:52:27 +0300

    KS-8-5 ингридиенты - добавлен joinToString, убраны arrayOfNulls

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -3,16 +3,11 @@ package org.example.lesson_8
 fun main() {
     println("Сколько ингридиентов вы планируете ввести? введите число")
     val countOfIngredients = readln().toInt()
-    val arrayOfIngredients = arrayOfNulls<String>(countOfIngredients)
-    var message = ""
+    val arrayOfIngredients = Array(countOfIngredients) {""}
+
     for (i in 0 until arrayOfIngredients.size) {
         println("Введите элемент ${i + 1}")
         arrayOfIngredients[i] = readln()
     }
-    for (i in arrayOfIngredients) {
-        message += i
-        message += ", "
-    }
-    message = message.dropLast(2)
-    println(message)
+    println(arrayOfIngredients.joinToString(separator = ", "))
 }
