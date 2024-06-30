56d325d726c5d0c2faa769d5985736fa6aec15d8
Lastbaks <missouri@inbox.ru>
2024-06-30 08:29:53 +0300

    KS-10-4 - игра в кости без глобальных переменных

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -4,45 +4,45 @@ import kotlin.random.Random
 
 const val MINIMAL_VALUE = 1
 const val MAXIMUN_VALUE = 7
-var countOfHumanWins = 0
-var readyToGame = true
 
 fun main() {
 
     println("Добро пожаловать в игру \"Кости\"")
-    while (readyToGame) {
-        round()
-    }
-    println("Игра закончена. Вы победили в $countOfHumanWins раундах")
+    val countOfWins = round()
+    println("Игра закончена. Вы победили в $countOfWins раундах")
 }
 
-fun round() {
-    val humanResult: Int
-    val machineResult: Int
+fun round(): Int {
+    var humanResult: Int
+    var machineResult: Int
+    var ready = true
+    var humanWinCount = 0
+    while (ready) {
+        println("Ваш ход")
+        humanResult = getRandomValue()
+        println("Сумма на ваших кубиках = $humanResult")
+        machineResult = getRandomValue()
+        println("Сумма на кубиках машины = $machineResult")
 
-    println("Ваш ход")
-    humanResult = getRandomValue()
-    println("Сумма на ваших кубиках = $humanResult")
-    machineResult = getRandomValue()
-    println("Сумма на кубиках машины = $machineResult")
+        if(humanResult > machineResult) {
+            println("Победило человечество")
+            humanWinCount += 1
+        } else if (machineResult > humanResult) {
+            println("Победили машины")
+        } else {
+            println("Ничья. человечество и машины ушли зализывать раны и готовиться к новому сражению")
+        }
+        println("Желаете сыграть еще раз?")
+        when (readln()) {
+            "да" -> ready = true
+            "нет" -> ready = false
+            else -> {
+                println( "вы ответили что то невразумительное, видимо вы в азарте боя. Игра начинается снова!")
 
-    if(humanResult > machineResult) {
-        println("Победило человечество")
-        countOfHumanWins += 1
-    } else if (machineResult > humanResult) {
-        println("Победили машины")
-    } else {
-        println("Ничья. человечество и машины ушли зализывать раны и готовиться к новому сражению")
-    }
-    println("Желаете сыграть еще раз?")
-    when (readln()) {
-        "да" -> readyToGame = true
-        "нет" -> readyToGame = false
-        else -> {
-           println( "вы ответили что то невразумительное, видимо вы в азарте боя. Игра начинается снова!")
-
+            }
         }
     }
+    return humanWinCount
 }
 
 fun getRandomValue(): Int {
