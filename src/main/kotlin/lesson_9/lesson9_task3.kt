a2e6923a34eed7cef6430d0f13af614d2267fbad
Lastbaks <missouri@inbox.ru>
2024-07-01 07:30:29 +0300

    KS-9-3 - рецепты. вариант с использованием метода map у списков

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_9
 
 fun main() {
-    val ingredients: MutableMap<String, Int> =
-        mutableMapOf("яйцо (шт)" to 2, "молоко (мл)" to 50, "сливочное масло (грамм)" to 15)
-    val countOfPortions: Int
+    val ingredientName = listOf("яйцо (шт)", "молоко (мл)", "сливочное масло (грамм)" )
+    val ingredientCount = listOf(2, 50, 15)
+    var countOfPortions: Int = 1
+    val countInRecept = ingredientCount.map { it * countOfPortions }
     println("Рецепт: Омлет")
     println("Какое количество порций вы хотите приготовить?")
     countOfPortions = readln().toInt()
     println("На $countOfPortions порций вам понадобится:")
-    for ((key, value ) in ingredients) {
-        println("$key - ${value * countOfPortions}")
+    for (i in 0 until  ingredientName.size) {
+        println("${ingredientName[i]} - ${countInRecept[i]}")
     }
 }
