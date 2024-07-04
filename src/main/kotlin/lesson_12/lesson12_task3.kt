4c0aad839a67e1b5ade111eef1f775d11b1671a1
Lastbaks <missouri@inbox.ru>
2024-07-04 22:03:19 +0300

    KS-12-3 - Данные о погоде - версия c градусами кельвина

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_12
+
+class WeatherDataVersion3(
+    _dayTemperature: Int,
+    _nightTemperature: Int,
+    _precipitation: Boolean
+) {
+    var dayTemperature = _dayTemperature
+    var nightTemperature = _nightTemperature
+    var precipitation = _precipitation
+
+    fun printWeatherInfo() {
+        println("Температура днем: ${dayTemperature - 273} градусов цельсия")
+        println("Температура ночью: ${nightTemperature - 273} градусов цельсия")
+        if (precipitation) {
+            println("Небольшой дождь")
+        } else {
+            println("Без осадков")
+        }
+    }
+}
+
+fun main() {
+    val june13 = WeatherDataVersion3(
+        301, 294, true
+    )
+    june13.printWeatherInfo()
+}
