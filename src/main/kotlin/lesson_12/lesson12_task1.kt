6850bbf85d7a75b0186365a8e619cd2aa85fa13c
Lastbaks <missouri@inbox.ru>
2024-07-05 07:23:11 +0300

    KS-12-1 - Данные о погоде. Переименована булевая переменная

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -4,12 +4,12 @@ class WeatherData {
 
     var dayTemperature: Int = 24
     var nightTemperature: Int = 13
-    var precipitation: Boolean = false
+    var isRainy: Boolean = false
 
     fun printWeatherInfo() {
         println("Температура днем: $dayTemperature градусов")
         println("Температура ночью: $nightTemperature градусов")
-        if (precipitation) {
+        if (isRainy) {
             println("Небольшой дождь")
         } else {
             println("Без осадков")
@@ -22,12 +22,12 @@ fun main() {
     val june10 = WeatherData()
     june10.dayTemperature = 30
     june10.nightTemperature = 18
-    june10.precipitation = true
+    june10.isRainy = true
 
     val june11 = WeatherData()
     june11.dayTemperature = 27
     june11.nightTemperature = 16
-    june11.precipitation = false
+    june11.isRainy = false
 
     june10.printWeatherInfo()
     june11.printWeatherInfo()
