9c377f1acb94318bd88e8808cbd47e2d72662f6f
Lastbaks <missouri@inbox.ru>
2024-07-07 00:36:52 +0300

    KS-13-3 - Телефонный справочник с пятью объектами

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,63 @@
+package org.example.lesson_13
+
+class ContactsVersion3(
+    val id: Int,
+    var name: String,
+    var company: String? = null,
+    var phoneNumber: Long
+) {
+    fun printContactInformation() {
+        println(
+            """
+            - Имя: $name
+            - Номер: $phoneNumber
+            - Компания: ${company ?: "не указано"}
+        """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val contact1 = ContactsVersion3(
+        id = 1,
+        name = "Джонни Депп",
+        phoneNumber = 89365612482,
+    )
+    val contact2 = ContactsVersion3(
+        id = 1,
+        name = "Киллиан Мерфи",
+        phoneNumber = 89365612482,
+    )
+    val contact3 = ContactsVersion3(
+        id = 1,
+        name = "Билл Найи",
+        company = "null",
+        phoneNumber = 89365612482,
+    )
+    val contact4 = ContactsVersion3(
+        id = 1,
+        name = "Марго Робби",
+        company = "Columbia pictures",
+        phoneNumber = 89365612482,
+    )
+    val contact5 = ContactsVersion3(
+        id = 1,
+        name = "Карла Гуджино",
+        company = "Paramount",
+        phoneNumber = 89365612482,
+    )
+
+    val contactList = mutableListOf<ContactsVersion3>()
+    contactList.add(contact1)
+    contactList.add(contact2)
+    contactList.add(contact3)
+    contactList.add(contact4)
+    contactList.add(contact5)
+
+    contactList.forEach {
+        if (it.company != null) {
+            println(it.company)
+        }
+    }
+}
+
