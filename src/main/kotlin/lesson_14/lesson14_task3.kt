9ef0ced2237733d476c3a14fbcf186a24457f3e7
Lastbaks <missouri@inbox.ru>
2024-07-10 19:18:24 +0300

    KS-14-3 - фигуры (круги и прямоугольники)

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -1,60 +1,66 @@
 package org.example.lesson_14
 
+import kotlin.math.PI
+import kotlin.math.round
+
+const val BLACK_COLOR: String = "Черный"
+const val WHITE_COLOR: String = "Белый"
+
 abstract class Figure(
-    open var color: String = "Черный"
+
+    open var color: String = BLACK_COLOR
 ) {
-    abstract fun getArea(): Float
+    abstract fun getArea(): Double
 
-    abstract fun getPerimeter(): Float
+    abstract fun getPerimeter(): Double
 }
 
-class Circle(color: String, val radius: Float,
+class Circle(
+    color: String, val radius: Double,
 ) : Figure() {
     override var color = color
-    override fun getArea(): Float {
-        return 3.14f * (radius * radius)
+    override fun getArea(): Double {
+        return PI * (radius * radius)
     }
 
-    override fun getPerimeter(): Float {
-        return (3.14f * 2 * radius)
+    override fun getPerimeter(): Double {
+        return (PI * 2 * radius)
     }
 }
 
-class Rectangle(color: String, val width: Float, val height: Float
+class Rectangle(
+    color: String, val width: Double, val height: Double
 ) : Figure() {
     override var color = color
-    override fun getArea(): Float {
+    override fun getArea(): Double {
         return (width * height)
     }
 
-    override fun getPerimeter(): Float {
+    override fun getPerimeter(): Double {
         return ((width + height) * 2)
     }
 }
 
 fun main() {
     val figures = mutableListOf<Figure>()
-    val circle1 = Circle("Черный", 20.0f)
-    val circle2 = Circle("Белый", 15.0f)
-    val rectangle1 = Rectangle("Черный", 2f, 3f)
-    val rectangle2 = Rectangle("Белый", 5f, 7f)
+    val circle1 = Circle(BLACK_COLOR, 20.0)
+    val circle2 = Circle(WHITE_COLOR, 15.0)
+    val rectangle1 = Rectangle(BLACK_COLOR, 2.0, 3.0)
+    val rectangle2 = Rectangle(WHITE_COLOR, 5.0, 7.0)
 
     figures.add(circle1)
     figures.add(circle2)
     figures.add(rectangle1)
     figures.add(rectangle2)
 
-    var sumOfArea: Float = 0f
-    var sumOfPerimeter: Float = 0f
+    var sumOfArea = 0.0
+    var sumOfPerimeter = 0.0
     figures.forEach {
-        if (it.color == "Черный") {
-            sumOfPerimeter += it.getArea()
-        }
-
-        if (it.color == "Белый") {
-            sumOfArea += it.getPerimeter()
+        when (it.color) {
+            BLACK_COLOR -> sumOfPerimeter += it.getArea()
+            WHITE_COLOR -> sumOfArea += it.getPerimeter()
         }
     }
-    println("Сумма периметров всех черных фигур равна $sumOfPerimeter")
-    println("Сумма площадей всех белых фигур равна $sumOfArea")
+    println("Сумма периметров всех черных фигур равна ${round(sumOfPerimeter * 100) / 100}")
+    println("Сумма площадей всех белых фигур равна ${round(sumOfArea * 100) / 100}")
 }
