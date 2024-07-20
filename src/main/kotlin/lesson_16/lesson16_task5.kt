355d5ff740a4c8096a40af3bf10b2f21c9bed28f
Lastbaks <missouri@inbox.ru>
2024-07-20 20:15:35 +0300

    KS-16-5-Класс игрока с методами получения урона и лечения

---
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -0,0 +1,43 @@
+package org.example.lesson_16
+
+const val HEALTH_POTION = 20
+    class Player(
+    val name: String,
+    private var damage: Int,
+    private var health: Int,
+    private var isDead: Boolean,
+) {
+
+    fun takeDamage(enemyDamage: Int) {
+        health -= enemyDamage
+        if (health <= 0) {
+            death()
+        }
+        println("Ударом нанесен урон $enemyDamage единиц. Осталось $health единиц жизни")
+    }
+
+    fun takeHealthPotion() {
+        if(!isDead) {
+            health += HEALTH_POTION
+            println("$name выпил лекарство.  Осталось $health единиц жизни")
+        } else {
+            println("Игрок мертв. Лечение невозможно")
+        }
+    }
+
+    private fun death() {
+        isDead = true
+        health = 0
+        damage = 0
+    }
+}
+
+fun main() {
+    val player1 = Player("Hetomukira", 10, 100, false)
+    player1.takeDamage(70)
+    player1.takeHealthPotion()
+    player1.takeDamage(50)
+    player1.takeHealthPotion()
+    player1.takeDamage(80)
+    player1.takeHealthPotion()
+}
