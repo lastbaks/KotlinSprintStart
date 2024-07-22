bed5331b2573f233faa2ecc2917f6a02eef003cb
Lastbaks <missouri@inbox.ru>
2024-07-22 18:41:08 +0300

    KS-17-5-Класс User без первичной инициализации

--- src/main/kotlin/lesson_17/lesson17_task5.kt
+++ src/main/kotlin/lesson_17/lesson17_task5.kt
@@ -3,17 +3,20 @@ package org.example.lesson_17
 class User(userLogin: String, userPassword: String) {
     var userLogin: String = userLogin
         get() = field
+        set(value) {
+            field = value
+            println("Логин успешно изменен")
+        }
     var userPassword: String = userPassword
         get() = getPassword(field)
         set(value) {
             println("Вы не можете изменять пароль")
         }
 
-    fun getPassword(pass: String): String {
-        val listOfChar = pass.toMutableList()
-        listOfChar.fill('*')
-        val newStroka = listOfChar.joinToString("")
-        return newStroka
+    private fun getPassword(field: String): String {
+        var newStroke = ""
+        newStroke += "*".repeat(field.length)
+        return newStroke
     }
 }
 
@@ -23,5 +26,6 @@ fun main() {
     println("Пароль: ${user1.userPassword}")
     user1.userPassword = "myNewPassword"
     println("Пароль: ${user1.userPassword}")
-
+    user1.userLogin = "Luntik-multik"
+    println("Имя пользователя: ${user1.userLogin}")
 }
