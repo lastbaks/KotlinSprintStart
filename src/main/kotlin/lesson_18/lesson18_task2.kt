5a06534d14389e71e7ef85266d6a14f296cd981f
Lastbaks <missouri@inbox.ru>
2024-07-23 19:06:18 +0300

    KS-18-2-игральные кубики на 4, 6 и 8 граней

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -1,34 +1,36 @@
 package org.example.lesson_18
 
-abstract class Dice() {
+abstract class Dice(
+    val sideCount: Int,
+) {
     abstract fun getRandom(): Int
 }
 
-class FourSideDice() : Dice() {
+class FourSideDice(sideCount: Int) : Dice(sideCount) {
     override fun getRandom(): Int {
-        val numbers = (1..4)
+        val numbers = (1..sideCount)
         return numbers.random()
     }
 }
 
-class SixSideDice() : Dice() {
+class SixSideDice(sideCount: Int) : Dice(sideCount) {
     override fun getRandom(): Int {
-        val numbers = (1..6)
+        val numbers = (1..sideCount)
         return numbers.random()
     }
 }
 
-class EightSideDice() : Dice() {
+class EightSideDice(sideCount: Int) : Dice(sideCount) {
     override fun getRandom(): Int {
-        val numbers = (1..8)
+        val numbers = (1..sideCount)
         return numbers.random()
     }
 }
 
 fun main() {
-    val forSideDice = FourSideDice()
-    val sixSideDice = SixSideDice()
-    val eightSideDice = EightSideDice()
+    val forSideDice = FourSideDice(4)
+    val sixSideDice = SixSideDice(6)
+    val eightSideDice = EightSideDice(8)
 
     val listOfDices = listOf(forSideDice, sixSideDice, eightSideDice)
     showAllDices(listOfDices)
