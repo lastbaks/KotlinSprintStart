f11e8ed6ddb1b70e0fae916a0e4687efa8f41ca6
Lastbaks <missouri@inbox.ru>
2024-07-27 23:29:03 +0300

    KS-19-4-класс танка с видами зарядов

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -4,20 +4,20 @@ enum class Ammo(val baseDamage: Int?, val label: String) {
     BLUE_BULLETS(5, "Базовый калибр"),
     GREEN_BULLETS(10, "Бронебойный"),
     RED_BULLETS(20, "Кумулятивный"),
-    NO_BULLETS(null, "заряд отсутствует"),
+//    NO_BULLETS(null, "заряд отсутствует"),
 }
 
 class Tank(
     var typeAmmo: Ammo? = null
 ) {
-    fun changeAmmo(type: Ammo) {
+    fun insertAmmo(type: Ammo) {
         typeAmmo = (when (type) {
             Ammo.BLUE_BULLETS -> Ammo.BLUE_BULLETS
             Ammo.GREEN_BULLETS -> Ammo.GREEN_BULLETS
             Ammo.RED_BULLETS -> Ammo.RED_BULLETS
-            Ammo.NO_BULLETS -> Ammo.NO_BULLETS
+//            Ammo.NO_BULLETS -> Ammo.NO_BULLETS
         })
-        println("Изменен тип боеприпасов на ${typeAmmo!!.label}")
+        println("Установлен тип боеприпасов на ${typeAmmo!!.label}")
     }
 
     fun takeShoot() {
@@ -27,13 +27,21 @@ class Tank(
             println("Выстрел невозможен. Зарядите орудие")
         }
     }
+
+    fun removeAmmo() {
+        typeAmmo = null
+    }
 }
 
 fun main() {
     val tank1 = Tank()
     tank1.takeShoot()
-    tank1.changeAmmo(Ammo.RED_BULLETS)
+    tank1.insertAmmo(Ammo.RED_BULLETS)
     tank1.takeShoot()
-    tank1.changeAmmo(Ammo.GREEN_BULLETS)
+    tank1.insertAmmo(Ammo.GREEN_BULLETS)
+    tank1.takeShoot()
+    tank1.removeAmmo()
+    tank1.takeShoot()
+    tank1.insertAmmo(Ammo.BLUE_BULLETS)
     tank1.takeShoot()
 }
