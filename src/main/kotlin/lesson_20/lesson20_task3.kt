8bc54b9e5cd0a646b1560021efcb265a5b0452c3
Lastbaks <missouri@inbox.ru>
2024-07-28 21:45:02 +0300

    KS-20-3 класс игрока и лямбда с ключем для двери

---
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_20
+
+class Player20_3(
+    val name: String,
+    var redKey: Boolean,
+)
+
+fun main() {
+
+    val openTheDoor: (Player20_3) -> Unit = { player ->
+
+        if (player.redKey) {
+            println("Дверь открыта")
+        } else {
+            println("Нет красного ключа")
+        }
+    }
+
+    val player1 = Player20_3("Hero", false)
+    openTheDoor(player1)
+    player1.redKey = true
+    openTheDoor(player1)
+}
