451cdd9e7410e497769ef1ab4ec1b7053080f3ba
Lastbaks <missouri@inbox.ru>
2024-07-31 19:16:09 +0300

    KS-21-2 extension функция подсчета суммы четных чисел списка

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -1,18 +1,9 @@
 package org.example.lesson_21
 
-fun List<Int>.evenNumbersSum(numbers: List<Int>): Int {
-    val evenNumbers = mutableListOf<Int>()
-
-    numbers.forEach {
-        if (it % 2 == 0) {
-            evenNumbers.add(it)
-        }
-    }
-    return evenNumbers.sum()
-}
+fun List<Int>.evenNumbersSum() = this.filter { it % 2 == 0 }.sum()
 
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-    val evenNumberSum = numbers.evenNumbersSum(numbers)
-    println(evenNumberSum)
+    val evenNumbersSum = numbers.evenNumbersSum()
+    println(evenNumbersSum)
 }
