9621ae19b10faf4323b2a69da1de3cef098552aa
Lastbaks <missouri@inbox.ru>
2024-08-04 16:09:09 +0300

    KS-21-5 - проверка максимального навыка

--- src/main/kotlin/lesson_21/lesson21_task5.kt
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -1,21 +1,14 @@
 package org.example.lesson_21
 
-class Map(
-    val name: String,
-    var skills: MutableMap<String, Int> = mutableMapOf(
+fun Map<String, Int>.maxCategory() = maxByOrNull { it.value }?.key
+
+fun main() {
+
+    val skills: MutableMap<String, Int> = mutableMapOf(
         "meleeWeapon" to 5,
         "nightVision" to 5,
         "hunting" to 4,
     )
-)
 
-fun Map.maxCategory() = skills.maxByOrNull { it.value }?.key
-
-fun main() {
-    val player1 = Map("Rudolf")
-
-    println(player1.maxCategory())
-    player1.skills["hunting"] = 6
-    println(player1.maxCategory())
+    println(skills.maxCategory())
 }
-
