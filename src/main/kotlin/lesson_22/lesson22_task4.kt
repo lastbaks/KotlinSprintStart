75f1b6b59cb53d934a7da1ed378bb70bcdb59e14
Lastbaks <missouri@inbox.ru>
2024-08-07 17:50:37 +0300

    KS-22-4 - имитация загрузки данных с сервера

--- src/main/kotlin/lesson_22/lesson22_task4.kt
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -1,19 +1,22 @@
 package org.example.lesson_22
 
-data class MainScreenState(
-    var data: String = "Нет данных",
-    var isLoading: Boolean = false,
-)
+class ViewModel {
+    data class MainScreenState(
+        var data: String = "Нет данных",
+        var isLoading: Boolean = false,
+    )
 
-fun loadData() {
-    var currentState = MainScreenState()
-    println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
-    currentState = currentState.copy(isLoading = true)
-    println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
-    currentState = currentState.copy(data = "Пункт назначения - Магратея", isLoading = false)
-    println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
+    fun loadData() {
+        var currentState = MainScreenState()
+        println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
+        currentState = currentState.copy(isLoading = true)
+        println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
+        currentState = currentState.copy(data = "Пункт назначения - Магратея", isLoading = false)
+        println("Состояние загрузки: ${currentState.isLoading}, переданные данные: ${currentState.data}")
+    }
 }
 
 fun main() {
-    loadData()
+    val viewModel = ViewModel()
+    viewModel.loadData()
 }
